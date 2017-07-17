cba6cecf8fd50638099eab4173419b3ef81e59ba
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-07-17 23:31:12 +0000

    version 1 complete

--- wnc_todos.js
+++ wnc_todos.js
@@ -1,3 +1,13 @@
 //build version 1
 
+var todos = ['item 1', 'item 2', 'item 3']
 
+todos.push('item 4')
+
+todos.push('item 5')
+
+todos[2] = 'item 3 updated'
+
+todos.splice(0,1)
+
+console.log('My todos:',todos)
