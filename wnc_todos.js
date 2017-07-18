bc8efa5e6529dd15a65983fc16cc60cac21a6d78
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-07-18 00:42:12 +0000

    version 2 - functions almost complete

--- wnc_todos.js
+++ wnc_todos.js
@@ -1,5 +1,5 @@
 //build version 1
-
+/*
 var todos = ['item 1', 'item 2', 'item 3']
 
 todos.push('item 4')
@@ -10,4 +10,37 @@ todos[2] = 'item 3 updated'
 
 todos.splice(0,1)
 
-console.log('My todos:',todos)
+console.log('My todos:',todos) 
+*/
+
+//build version 2 - functions
+var todos = ['item 1', 'item 2', 'item 3'];
+
+function displayTodos() {
+    console.log('My todos:',todos);
+}
+
+function addTodo(todo) {
+    todos.push(todo);
+    displayTodos();
+}
+
+function changeTodo(position, newValue) {
+ //todos[0] = 'some new value';
+ todos[position] = newValue;
+ displayTodos();
+}
+
+function deleteTodo(position) {
+    todos.splice(position, 1);
+    displayTodos();
+}
+
+addTodo('some stuff');
+addTodo('new stuff');
+changeTodo(0, 'changed');
+//changeTodo(0, 'changed again');
+
+
+//displayTodos();
+deleteTodo(2);
