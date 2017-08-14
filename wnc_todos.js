0f717dedacbe516b200c592e1df4176f37dc38a7
Dennis Daubney Jr <dennis.daubney@gmail.com>
2017-08-14 22:21:43 +0000

    finished version 10

--- wnc_todos.js
+++ wnc_todos.js
@@ -1,4 +1,4 @@
-//start version 9
+//start version 10
 var todoList = {
     todos: [],
     addTodo: function(todoText) { 
@@ -60,10 +60,8 @@ var handlers = { //handle different events/clicks
         changeTodoTextInput.value = '';
         view.displayTodos();
     },
-    deleteTodo: function() {
-        var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
-        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-        deleteTodoPositionInput.value = '';
+    deleteTodo: function(position) {
+        todoList.deleteTodo(position);
         view.displayTodos();
     },
     toggleCompleted: function() {
@@ -94,8 +92,31 @@ var view = {
                 todoTextWithCompletion = '( ) ' + todo.todoText;
             }
             
+            todoLi.id = i;
             todoLi.textContent = todoTextWithCompletion;
+            todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
         }
+    },
+    createDeleteButton: function () {
+        var deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.className = 'deleteButton';
+        return deleteButton;
+    },
+    setUpEventListeners: function() {
+        var todosUl = document.querySelector('ul');
+    
+        todosUl.addEventListener('click', function(event) {
+        
+        var elementClicked = event.target;
+        
+        if (elementClicked.className === 'deleteButton') {
+           handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+       
+    }
+});    
     }
 };
+
+view.setUpEventListeners();
